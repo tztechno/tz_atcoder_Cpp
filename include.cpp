24cfd064771076435d8c15548ae74a2e794df068
stpeteishii <47345279+tztechno@users.noreply.github.com>
2023-11-06 11:55:57 +0900

    Update include.cpp

--- include.cpp
+++ include.cpp
@@ -1,5 +1,19 @@
     #include <bits/stdc++.h>
     using namespace std;
-    
+
+/* これは
     #include <bits/stdc++.h> is a commonly used header that includes many standard C++ libraries. 
     using namespace std; is used to bring the entire std namespace into the current scope. 
+複数行コメントアウトです */
+
+##################################
+        
+    int main() {
+        string n, s;
+        cin >> n >> s; 
+
+/* これは
+    string n, s; declares two string variables, n and s, which will be used to store the input strings.
+    cin >> n >> s; reads two strings from the standard input.    
+複数行コメントアウトです */   
+
