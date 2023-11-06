d807a79c5146344e1cb2026e5b9b6f7a2cfca88d
stpeteishii <47345279+tztechno@users.noreply.github.com>
2023-11-06 12:01:36 +0900

    Create regex_search.cpp

---
+++ regex_search.cpp
@@ -0,0 +1,9 @@
+   cout << (regex_search(s,regex(".*ab|ba.*")) ? "Yes":"No") << endl;
+    }
+
+/* これは
+    regex_search(s, regex(".*ab|ba.*")) checks if the regular expression ".ab|ba." matches any part of the string s. 
+   コメントアウトです */
+
+/* これは
+   コメントアウトです */
