d4e8fb4b8ac54735ef16757a05fe0d51e3183fb5
stpeteishii <47345279+tztechno@users.noreply.github.com>
2023-11-06 11:44:01 +0900

    Create include.cpp

---
+++ include.cpp
@@ -0,0 +1,5 @@
+    #include <bits/stdc++.h>
+    using namespace std;
+    
+    #include <bits/stdc++.h> is a commonly used header that includes many standard C++ libraries. 
+    using namespace std; is used to bring the entire std namespace into the current scope. 
