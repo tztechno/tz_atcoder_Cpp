bb566767143c0340776d62c19e768b1ec2c8566d
stpeteishii <47345279+tztechno@users.noreply.github.com>
2023-11-07 14:11:56 +0900

    Update include.cpp

--- include.cpp
+++ include.cpp
@@ -1,10 +1,22 @@
+
+##################################
+
+/* これは
+
+    #include <iostream> は、C++の標準入出力に関連するヘッダーファイルをインクルードします。標準入力(cin)と標準出力(cout)を扱うためのクラスや関数が含まれています。
+    #include <vector> は、C++の標準ライブラリで提供される動的配列（ベクトル）を使用するためのヘッダーファイルをインクルードします。
+    
+コメントアウトです */
+
+##################################
+        
     #include <bits/stdc++.h>
     using namespace std;
 
 /* これは
     #include <bits/stdc++.h> is a commonly used header that includes many standard C++ libraries. 
     using namespace std; is used to bring the entire std namespace into the current scope. 
-複数行コメントアウトです */
+コメントアウトです */
 
 ##################################
         
@@ -15,5 +27,7 @@
 /* これは
     string n, s; declares two string variables, n and s, which will be used to store the input strings.
     cin >> n >> s; reads two strings from the standard input.    
-複数行コメントアウトです */   
+コメントアウトです */   
 
+##################################
+        
