bdb6e55f1be402829724751d155c3219ce3b85a4
stpeteishii <47345279+tztechno@users.noreply.github.com>
2023-11-26 13:58:15 +0900

    Create hello.cpp

---
+++ hello.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <unordered_map>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+
+    if (s == "Hello,World!") {
+        cout << "AC" << endl;
+    } else {
+        cout << "WA" << endl;
+    }
+
+    return 0;
+}
