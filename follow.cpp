f7b9fc201e713f1e8d0379e4d8e3092772700f58
stpeteishii <47345279+tztechno@users.noreply.github.com>
2023-12-20 12:22:57 +0900

    Create follow.cpp

---
+++ follow.cpp
@@ -0,0 +1,14 @@
+//ABC182_A
+//Follow
+
+#include <iostream>
+
+int main() {
+    
+    int A, B;
+    std::cin >> A >> B;
+    int MAXB=2*A+100;
+    std::cout << MAXB-B << std::endl;
+
+    return 0;
+}
