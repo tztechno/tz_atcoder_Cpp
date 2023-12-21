a7e9703f682824f92b8736dc5e5a01101e0cd17d
stpeteishii <47345279+tztechno@users.noreply.github.com>
2023-12-21 16:13:30 +0900

    Create rotation.cpp

---
+++ rotation.cpp
@@ -0,0 +1,18 @@
+#ABC181_A
+#rotation
+#input= 5
+
+
+#include <iostream>
+#include <cmath>
+
+int main() {
+    int N;
+    std::cin >> N;
+    if (N%2==0) {
+        std::cout << "White" << std::endl;
+    } else {
+        std::cout << "Black" << std::endl;
+    }
+    return 0;
+}
