e109f278bf9a514bb519befe77607150563bbab3
stpeteishii <47345279+tztechno@users.noreply.github.com>
2024-01-01 00:17:24 +0900

    Create not.cpp

---
+++ not.cpp
@@ -0,0 +1,14 @@
+//ABC178_A 
+//Not
+
+#include <iostream>
+
+int main() {
+    int N;
+    std::cin >> N;
+    if (N==1) {
+        std::cout << 0 << std::endl;
+    } else {
+        std::cout << 1 << std::endl;        
+    }
+}
