8a9cc1df4d232c59b940b161490c3a1075eac4f4
stpeteishii <47345279+tztechno@users.noreply.github.com>
2024-02-20 17:09:12 +0900

    Create multiplication.cpp

---
+++ multiplication.cpp
@@ -0,0 +1,41 @@
+//abc169_a multiplication.cpp
+################################
+################################
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b;
+    cin>>a>>b;
+    cout<<a*b<<endl;
+}
+################################
+#include <iostream>
+using namespace std;
+int main() {
+  int a;
+  int b;
+  cin>>a;
+  cin>>b;
+  int result =a*b;
+  cout<<result<<endl;
+  return 0;
+}
+################################
+#include <iostream>
+int main() {
+using namespace std;
+int A,B,result;
+cin>>A>>B;
+result=A*B;
+cout<<result;
+    return 0;
+}
+################################
+#include <iostream>
+int main() {
+    int A, B;
+    std::cin >> A >> B;
+    std::cout << A*B << std::endl;
+    return 0;
+}
+################################
