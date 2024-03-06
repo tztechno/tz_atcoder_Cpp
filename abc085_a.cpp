b259bf2a407ae31feb7e4f9b36cd348c47cd5443
stpeteishii <47345279+tztechno@users.noreply.github.com>
2024-03-06 20:50:55 +0900

    Create abc085_a.cpp

---
+++ abc085_a.cpp
@@ -0,0 +1,48 @@
+//abc085_a.cpp
+###################################
+###################################
+string S2 = "2018" + S.substr(4);
+###################################
+###################################
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+  string s;
+  cin >> s;
+  s.at(3) = '8';
+  cout << s<< endl;
+}
+###################################
+#include<bits/stdc++.h>
+using namespace std;
+char a;
+string s;
+int main()
+{
+    cin>>a>>a>>a>>a>>s;
+    cout<<2018<<s<<endl;
+}
+###################################
+// LUOGU_RID: 149134370
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	string s;
+	cin>>s;
+	cout<<"2018/01/"<<s[8]<<s[9];
+	cout<<endl;
+    return 0;
+}
+###################################
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+    string S;
+    cin >> S;
+    string S2 = "2018" + S.substr(4);
+    cout << S2 << endl;
+    return 0;
+}
+###################################
