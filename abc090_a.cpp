a897b8525f1a601d09aaa0ffa3d734f305e0116b
stpeteishii <47345279+tztechno@users.noreply.github.com>
2024-03-11 14:29:46 +0900

    Create abc090_a.cpp

---
+++ abc090_a.cpp
@@ -0,0 +1,40 @@
+//abc090_a.cpp
+###################################
+###################################
+#include<iostream>
+using namespace std;
+
+int main(){
+    string A, B, C;
+    cin >> A >> B >> C;
+    cout << A[0] << B[1] << C[2] << '\n';
+
+    return 0;
+}
+###################################
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    string a,b,c;
+    cin>>a>>b>>c;
+    cout<<a[0]<<b[1]<<c[2]<<endl;
+    return 0;
+}
+###################################
+#include<iostream>
+using namespace std;
+int main()
+{
+	char a,s,d,f,g,h,j,k,l;
+	cin>>a>>s>>d>>f>>g>>h>>j>>k>>l;
+	cout<<a<<g<<l<<endl;
+ }
+###################################
+#include <bits/stdc++.h>
+using namespace std;
+int main (){
+  string a,b,c;
+  cin >> a >> b >> c;
+  cout << a[0] << b[1] << c[2];
+}
+###################################
