4a9c5be71f46b8c9a798a2dd4f6c3b91043681eb
stpeteishii <47345279+tztechno@users.noreply.github.com>
2024-03-15 14:24:15 +0900

    Create abc093.cpp

---
+++ abc093.cpp
@@ -0,0 +1,48 @@
+//abc093.cpp
+####################################
+####################################
+####################################
+####################################
+####################################
+####################################
+####################################
+#include<iostream>
+#include<algorithm>
+std::string s;
+int main(){
+	std::cin>>s;
+	std::sort(s.begin(),s.end());
+	if(s=="abc")puts("Yes");
+	else puts("No");
+} 
+####################################
+#include <iostream>
+#include <string>
+using namespace std;
+int main(){
+	string s;
+	cin >> s;
+    if(s == "abc"||s == "acb"||s == "bac"||s == "bca"||s == "cab"||s == "cba")
+	    	cout << "Yes" << endl;
+	else
+    	cout << "No" << endl;
+	return 0;
+}
+####################################
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string S;
+    cin >> S;
+
+    if (S.find("a") != string::npos && S.find("b") != string::npos && S.find("c") != string::npos) {
+        cout << "Yes" << endl;
+    } else {
+        cout << "No" << endl;
+    }
+
+    return 0;
+}
+####################################
