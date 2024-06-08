06daab8ff70623b176673aee9d94e37f5ad49045
stpeteishii <47345279+tztechno@users.noreply.github.com>
2024-06-08 16:58:46 +0900

    Create abc080_b.cpp

---
+++ abc080_b.cpp
@@ -0,0 +1,58 @@
+abc080_b.cpp
+##########################################
+##########################################
+##########################################
+##########################################
+##########################################
+##########################################
+[after, AC]
+#include <iostream>
+#include <string>
+int main() {
+    int M = 0;
+    std::string X;
+    std::cin >> X;
+    for (size_t i = 0; i < X.size(); ++i) {
+        char x = X[i];
+        M += x - '0'; // Convert character to integer
+    }
+    int num = std::stoi(X); // Convert string to integer
+    if (num % M == 0) {
+        std::cout << "Yes";      
+    } else {
+        std::cout << "No";      
+    }
+    return 0;
+}
+##########################################
+[before, WA]
+#include <iostream>
+#include <vector>
+int main() {
+    int M=0;
+    str X:
+    std::cin >> X;
+    std::cout << N << std::endl;
+    for (int i = 0; i < len(X); ++i) {
+        x=X[i]
+        M+=int(x)
+    }
+    if (int(X)%M==0 ){
+      std::cout << "Yes" ;      
+    }
+    else {
+      std::cout << "No" ;      
+    }
+    return 0;
+}
+##########################################
+[python]
+X=str(input())
+M=0
+for x in list(X):
+  M+=int(x)
+if int(X)%M==0:
+  print('Yes')
+else:
+  print('No')
+##########################################
